a80fbd1f849ee2f3853d3e74afa06a1fc103b1e3
Matthew <matthew_mcaleer@hotmail.com>
2022-08-23 18:54:54 +1000

    commit tree 8240bd494c30615c8bb541362bdf98a98e77d2fd

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,9 @@
 // write and export your action creator function here
+export function fetchCats() {
+  return (dispatch) => {
+    dispatch({ type: "LOADING_CATS" });
+    fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+      .then((response) => response.json())
+      .then((respJSON) => dispatch({ type: "ADD_CATS", cats: respJSON.images }));
+  };
+}
